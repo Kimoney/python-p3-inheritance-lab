899a121246ac395d3678c0c9430549cb779ffaa6
John <johntaykimani@gmail.com>
2024-03-05 13:49:46 +0300

    Completed Asignment

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 from user import User
+knowledge = list()
 
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, string):
+        self.string = string
+        self.knowledge.append(self.string)
